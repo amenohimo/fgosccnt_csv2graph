59b936b64fb434497c7669207b242c0766966fd8
ame <amenohimogamo@gmail.com>
2021-08-07 23:01:34 +0900

    Fixed the time display when running c2g_dae.bat

--- c2g_dae.bat
+++ c2g_dae.bat
@@ -21,15 +21,12 @@ set csv2graph.py="C:\path\to\csv2graph.py"
 rem Directory path to output the graph image
 set graphs_dir="C:\path\to\graphs"
 
-set yyyy=%date:~0,4%
-set mm=%date:~5,2%
-set dd=%date:~8,2%
 set time2=%time: =0%
 set hh=%time2:~0,2%
 set mn=%time2:~3,2%
 set ss=%time2:~6,2%
-set time=%yyyy%_%mm%_%dd%_%hh%_%mn%_%ss%
-echo グラフのプロットを開始します [%date% %time%]
+time=%hh%:%mn%:%ss%
+echo Start plotting the graph [%date% %time%]
 
 rem -a Output all kinds of graphs
 rem -w Outputs a graph that displays the value when the cursor is placed on the browser
